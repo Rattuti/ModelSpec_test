0b2cbdd7d8fd81421582151ac206a2c624af6184
Rattuti <i.ishiharar.96296@gmail.com>
2022-06-22 11:22:53 +0900

    test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -15,12 +15,20 @@ RSpec.describe Task, type: :model do
     end
 
     it "完了期限が未入力の場合、タスクのバリデーションが無効であること" do
+      task = Task.new(title: "test", description: "test", status: todo, deadline: nil)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline can't be blank"]
     end
 
-    it "完了期限が過去の日付の場合、タスクのバリデーションが無効であること" do
+    it "#完了期限が過去の日付の場合、タスクのバリデーションが無効であること" do
+      task = Task.new(title: "test", description: "test", status: todo, deadline: Day.yesterday)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline can't be past day"]
     end
 
-    it "完了期限が今日の日付の場合、タスクのバリデーションが有効であること" do
+    it "#完了期限が今日の日付の場合、タスクのバリデーションが有効であること" do
+      task = Task.new(title: "test", description: "test", status: todo, deadline: Day.today)
+      expect(task).to be_valid
     end
   end
 end
